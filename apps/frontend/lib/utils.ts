aaf792d5d9572380b735b180bcfc343601ca5c74
Abhishek Masne <abhishekmasne2015@gmail.com>
2025-08-26 06:19:23 +0530

    Complete end to end project with deployment

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+//export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://betteruptime.abhishek97.icu/api";
