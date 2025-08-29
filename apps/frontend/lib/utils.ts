f2ba4913bae7a3820f2486874ee927c3c1b3ddba
Abhishek Masne <abhishekmasne2015@gmail.com>
2025-08-30 01:31:41 +0530

    HTPP to HTTPS

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -6,4 +6,4 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 //export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://betteruptime.abhishek97.icu/api";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://betteruptime.abhishek97.icu/api";
